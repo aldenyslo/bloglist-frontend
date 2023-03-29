ba0d95eb3586e533b6c4f993f9788904ebf6cd10
drzukus <alden.ys.lo@gmail.com>
2023-03-29 13:16:23 -0700

    commit

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,8 @@
+import { createSlice } from "@reduxjs/toolkit"
+import loginService from "../services/login"
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: null,
+  reducers: 
+})
